09f7c92dcfc6e4e0000838fe7b00aacdff1fb2ef
euWna <112548635+euWna@users.noreply.github.com>
2022-12-01 04:39:53 +0900

    맛집 세부정보, 메뉴 불러오기까지

--- flask-server/app.py
+++ flask-server/app.py
@@ -10,10 +10,101 @@ DB = DBhandler()
 
 DB.__init__()
 
-
+#view(GET) functions
 @app.route("/")
 def index():
     return render_template("index.html")
     #return redirect(url_for('list_stores'))
 
-@app.route()
+@app.route("/CreateStore")
+def view_CreateStore():
+    return render_template("CreateStore.html")
+
+@app.route("/CreateMenu/<storename>")
+def view_CreateMenu(storename):
+    return render_template("CreateMenu.html", storename=storename)
+
+@app.route("/StoreList")
+def view_StoreList():
+    return render_template("StoreList.html")
+
+@app.route("/ReviewAll")
+def view_ReviewList():
+    return render_template("ReviewList.html")
+
+
+
+#Submit(POST) functions
+@app.route("/submit_store", methods=['POST'])
+def submit_store():
+    img_file=request.files["imgfile"]
+    if img_file:
+        img_file.save("./flask-server/static/img/"+img_file.filename)
+    data=request.form
+    storename=data["storename"]
+    
+    if(DB.insert_store(data["storename"],data, img_file.filename)) :
+        print("submit_store success")
+        #return render_template("CreateStore_update.html", result=data)
+        return redirect(url_for('Created_store',storename=storename))
+    else:
+        return "The submitted store already exists!"
+
+@app.route("/Created_store/<storename>")
+def Created_store(storename):
+    submitted = DB.get_store_byname(storename)
+    return render_template("CreateStore_update.html", submitted=submitted)
+
+@app.route("/submit_menu", methods=['POST'])
+def submit_menu():
+        img_file=request.files["imgfile"]
+        if img_file:
+            img_file.save("./flask-server/static/img/"+img_file.filename)
+        data=request.form
+        storename=data["storename"]
+
+        if(DB.insert_menu(storename, data, img_file.filename)):
+            print("submit_menu success")
+            return redirect(url_for('Created_menu', storename=storename))
+        else:
+            return "The submitted menu already exists!"
+
+@app.route("/Created_menu/<storename>")
+def Created_menu(storename):
+    menudata = DB.get_menus(storename)
+    #print(menudata.items())
+    #submitted = DB.get_menu_byname(storename)
+    return render_template("CreateMenu_update.html", storename=storename, menudata=menudata.items())
+
+@app.route("/StoreListView")
+def view_list():
+    page = request.args.get("page",0,type=int)
+    limit = 5
+
+    start_idx=limit*page
+    end_idx=limit*(page+1)
+    storedata = DB.get_stores()
+    tot_count = len(storedata)
+    storedata = dict(list(storedata.items())[start_idx:end_idx])
+
+    #print(tot_count, limit, page, int(tot_count/5)+1)
+    return render_template(
+        "StoreListView.html",
+        storedata=storedata.items(),
+        total=tot_count,
+        limit=limit,
+        page=page,
+        page_count=int(tot_count/limit)+1)
+
+@app.route("/StoreDetail/<storename>")
+def view_detail(storename):
+    storedata = DB.get_store_byname(str(storename))
+    menudata = DB.get_menus(str(storename))
+    #menudata = DB.get_menus()
+    #reviewdata = DB.get_reviews()
+    return render_template("StoreDetail.html", storedata=storedata, menudata=menudata.items())
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=True)
+    
--- flask-server/database.py
+++ flask-server/database.py
@@ -12,6 +12,53 @@ class DBhandler:
     #맛집 정보 입력 함수
     def insert_store(self,name,data,img_path):
         store_info ={
+            "storename" : data['storename'],
+            "location" : data['location'],
+            "phonenumber" : data['phonenumber'], 
+            "time1" : data['time1'],
+            "time2" : data['time2'],
+            "food" : data['food'],
+            "park" : data['park'],
+            "price1" : data['price1'],
+            "price2" : data['price2'],
+            "site" : data['site'],
+            "img_path" : "../static/image/"+img_path
+        }
+
+        if self.store_duplicate_check(name):
+            self.db.child("STORE").push(store_info)
+            #print(data,img_path)
+            return True
+        else:
+            return False
+        
+    # 맛집 정보 중복 체크 함수(insertStore에서 사용)
+    def store_duplicate_check(self, name):
+        stores = self.db.child("STORE").get()
+        for res in stores.each():
+            if res.val()['storename'] == name:
+                return False
+        return True
+
+    #전체 맛집 정보 가져오는 함수(StoreListView에서 사용)
+    def get_stores(self):
+        stores = self.db.child("STORE").get().val()
+        #print(stores)
+        return stores
+
+    #특정 맛집의 세부 정보 가져오는 함수
+    def get_store_byname(self,name):
+        stores = self.db.child("STORE").get()
+        target_value=""
+        for res in stores.each():
+            value = res.val()
+            if value['storename'] == name:
+                target_value=value
+                break
+        return target_value
+
+    def update_store(self,name,data,img_path):
+        store_info ={
             "name" : name,
             "storename" : data['storename'],
             "location" : data['location'],
@@ -27,65 +74,58 @@ class DBhandler:
         }
 
         if self.store_duplicate_check(name):
-            self.db.child("STORE").push(store_info)
-            print(data,img_path)
+            self.db.child("STORE").update(store_info)
+            #print(data,img_path)
             return True
         else:
-            return False
-        
-    # 맛집 정보 중복 체크 함수(insertStore에서 사용)
-    def store_duplicate_check(self, name):
-        stores = self.db.child("STORE").get()
-        for res in stores.each():
-            if res.key() == name:
-                return False
-        return True
-
-    #맛집 정보 가져오는 함수(StoreListView에서 사용)
-    def get_stores(self):
-        stores = self.db.child("STORE").get().val()
-        #print(stores)
-        return stores
-
-    def get_store_byname(self,name):
-        stores = self.db.child("STORE").get()
-        target_value=""
-        for res in stores.each():
-            value = res.val()
-            if value['storename'] == name:
-                target_value=value
-                break
-        return target_value
+            self.insert_store(self,name,data,img_path)
+            self.db.child("STORE").child(name).remove()
+            return True
 
 
-    def insert_menu(self,name,data,img_path):
+    def insert_menu(self,storename, menudata,img_path):
         menu_info ={
-            "food" : data['food'],
-            "money" : data['money'],
-            "nutrient" : data['nutrient'],
-            "img_path" : "static/image/"+img_path
+            "menuname" : menudata['menuname'],
+            "price" : menudata['price'],
+            "nutrient" : menudata['nutrient'],
+            "img_path" : "static/img/"+img_path
         }
-        if self.menu_duplicate_check(name):
-            self.db.child("MENU").child(name).push(menu_info)
-            print(data,img_path)
+        if self.menu_duplicate_check(storename, menudata['menuname']):
+            self.db.child("MENU").child(storename).child(menudata['menuname']).set(menu_info)
+            #print(data,img_path)
             return True
         else:
             return False
 
     # 메뉴 정보 중복 체크 함수(insertmenu에서 사용)
-    def menu_duplicate_check(self, name):
-        menus = self.db.child("MENU").get()
-        for res in menus.each():
-            if res.key() == name:
-                return False
+    def menu_duplicate_check(self, storename, menuname):
+        menudata = self.db.child("MENU").child(storename).get()
+        if isinstance(menudata.val(), type(None)):
+            #print("NONE")
+            return True
+        else:
+            for res in menudata.each():
+                if res.key() == menuname:
+                    return False
         return True
 
-    def get_menus(self):
-        menus = self.db.child("MENU").get().val()
+    # 해당 맛집의 메뉴들을 가져오는 함수 
+    def get_menus(self,storename):
+        menus = self.db.child("MENU").child(storename).get().val()
         #print(stores)
         return menus
 
-
+    #해당 맛집의 특정 메뉴의 정보를 가져오는 함수
+    def get_menu_byname(self,storename,menuname):
+        menus = self.db.child("MENU").child("storename").get()
+        target_key=""
+        for res in menus.each():
+            key = res.key()
+            if key == storename:
+                target_key=key
+                break
+        target_value=target_key.val()
+        return target_value
 
 
     def insert_review(self,name,data,img_path):
