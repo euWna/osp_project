2d99c596b241a2e3bde979e32c58eeb2592aeca8
euWna <112548635+euWna@users.noreply.github.com>
2022-12-08 22:31:22 +0900

    Revert "1206yj/create menu"

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ import ReviewAll from './pages/ReviewAll';
 import SignUp from './pages/SignUp';
 import StoreDetail from './pages/StoreDetail';
 import StoreListView from './pages/StoreListView';
-import CreateMenuShow from "./pages/CreateMenuShow"
 
 //App.js를  index.js에서 렌더링 중입니다!
 
@@ -39,7 +38,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/CreateStore" element={<CreateStore />}></Route>
-          <Route path="/CreateMenu/:storename" element={<CreateMenu />}></Route>
+          <Route path="/CreateMenu/:id" element={<CreateMenu />}></Route>
           <Route path="/CreateReview" element={<CreateReview />}></Route>
           <Route path="/Homepage" element={<Homepage />}></Route>
           <Route path="/Login" element={<Login />}></Route>
@@ -50,8 +49,6 @@ function App() {
           <Route path="/SignUp" element={<SignUp />}></Route>
           <Route path="/StoreDetail" element={<StoreDetail />}></Route>
           <Route path="/StoreListView" element={<StoreListView />}></Route>
-          <Route path="/StoreListViews" element={<StoreListView />}></Route>
-          <Route path="/CreateMenu/:storename/:food" element={<CreateMenu />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
