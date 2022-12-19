ae008ad196334b487e8f9b04dac8315dec74b3df
YJin33 <89703649+YJin33@users.noreply.github.com>
2022-12-19 17:25:36 +0900

    저장용 커밋(reviewAll 안되는 중)

--- src/pages/ReviewAll.js
+++ src/pages/ReviewAll.js
@@ -16,7 +16,6 @@ function ReviewSection() {
     // const reviewstorename = props.reviewstorename
     const [reviewdata, setData] = useState()
     var reviewarr = new Array();
-
     useEffect(() => {
         fetch(`/Review_send_data`, { //json 데이터를 받아옴
             headers: {
@@ -24,15 +23,26 @@ function ReviewSection() {
                 method: 'GET'
             }
         })
-            .then(response => response.json())
+            // .then(response => response.json())
             .then(jsonData => {
-                var length = Object.keys(jsonData).length
+                // stores = Object.entries(jsonData)
+                // stores = {}
+                var stores = setData(Object.entries(jsonData))
+                var length = Object.entries(jsonData).length
                 // var length = Object(jsonData).length
                 for (var i = 0; i < length; i++) { //식당 갯수만큼 반복
-                    reviewarr[i] = Object.values(jsonData)[i]
-                    reviewarr[i]["key"] = Object.keys(jsonData)[i] //키값 필요해서 가져옴
+
+                    //     var length2 = Object.entries.keys(jsonData).length//식당 당 user 수
+                    //     reviewarr[i] = Object.entries(jsonData)
+                    //     for (var j = 0; j < length2; j++) {
+                    //         reviewarr[i][j] = Object.entries(jsonData)[j] //storename
+                    //         reviewarr[i][j]["key"] = Object.entries(Object.keys(jsonData.reviewarr))[i] //키값 필요해서 가져옴}
                 }
-                setData(reviewarr)
+
+
+                // }
+                console.log(Object.values(jsonData)[1])
+                console.log("여기가 Object.values(jsonData)[1]")
             })
             .catch(
                 (err) => console.log(err))
@@ -66,6 +76,26 @@ function ReviewAll() {
     const [reviewdata, setData] = useState()
     var reviewarr = new Array();
 
+    // useEffect(() => {
+    //     fetch(`/Review_send_data`, { //json 데이터를 받아옴
+    //         headers: {
+    //             Accept: 'application/json',
+    //             method: 'GET'
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(jsonData => {
+    //             var length = Object.keys(jsonData).length
+    //             for (var i = 0; i < length; i++) { //식당 갯수만큼 반복
+    //                 reviewarr[i] = Object.values(jsonData)[i]
+    //                 reviewarr[i]["key"] = Object.keys(jsonData)[i] //키값 필요해서 가져옴
+    //             }
+    //             setData(reviewarr)
+    //         })
+    //         .catch(
+    //             (err) => console.log(err))
+    // }, [])
+    // console.log(reviewdata)
     useEffect(() => {
         fetch(`/Review_send_data`, { //json 데이터를 받아옴
             headers: {
@@ -75,17 +105,27 @@ function ReviewAll() {
         })
             .then(response => response.json())
             .then(jsonData => {
-                var length = Object.keys(jsonData).length
-                for (var i = 0; i < length; i++) { //식당 갯수만큼 반복
-                    reviewarr[i] = Object.values(jsonData)[i]
-                    reviewarr[i]["key"] = Object.keys(jsonData)[i] //키값 필요해서 가져옴
-                }
-                setData(reviewarr)
+                // stores = Object.keys(jsonData)
+                // var length = Object.entries(jsonData).length
+                // var length = Object(jsonData).length
+                // for (var i = 0; i < length; i++) { //식당 갯수만큼 반복
+                //     var length2 = Object.entries.keys(jsonData).length//식당 당 user 수 
+                //     reviewarr[i] = Object.entries(jsonData)
+                //     for (var j = 0; j < length2; j++) {
+                //         reviewarr[i][j] = Object.entries(jsonData)[j] //storename
+                //         reviewarr[i][j]["key"] = Object.entries(Object.keys(jsonData.reviewarr))[i] //키값 필요해서 가져옴}
+                //     }
+
+                setData(Object.values.keys(jsonData))
+                // }
+                console.log(Object.values.keys(jsonData)[0])
+                console.log("여기가 Object.values(jsonData)[1]")
             })
             .catch(
                 (err) => console.log(err))
     }, [])
     console.log(reviewdata)
+    console.log(typeof (reviewdata))
     return (
         <div className={styles.default}>
             <div className={styles.body}>
