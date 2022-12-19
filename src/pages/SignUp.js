dc98bf47d5cb3cc30bf265cddc943530831f29a9
zoo0YOON <yjy011123@ewhain.net>
2022-12-19 20:19:31 +0900

    css 수정?

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -8,7 +8,7 @@ import NavBar from "../component/NavBar";
 
 function SignUp() {
     const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
   
     // const onNameHandler = (event) => {
@@ -29,11 +29,23 @@ function SignUp() {
         <form action="/SignUp_post" method="POST" enctype="multipart/form-data">
           <div id={styles.table}>
             <div class={styles.row}>
-              <span class={`${styles.cell} ${styles.col1}`}><input name="nickname" type="text" placeholder="이름"   className="loginregister__input"/></span>
+              <span class={`${styles.cell} ${styles.col1}`}>이름</span>
+              <span class={`${styles.cell} ${styles.col2}`}><input name="nickname" type="text" placeholder="이름"   className="loginregister__input"/></span>
+            </div>  
+            <div class={styles.row}> 
+              <span class={`${styles.cell} ${styles.col1}`}>이메일</span>         
               <span class={`${styles.cell} ${styles.col2}`}><input name="email" type="email" placeholder="이메일"  className="loginregister__input"/></span>
-              <span class={`${styles.cell} ${styles.col1}`}><input name="pwd" type="password" placeholder="비밀번호"   classNames="loginregister__input"/></span>
-              <span class={`${styles.cell} ${styles.col1}`}><input type="submit" class={styles.btn_submit} value="회원가입" /></span>
             </div>
+            <div class={styles.row}> 
+              <span class={`${styles.cell} ${styles.col1}`}>비밀번호</span>
+              <span class={`${styles.cell} ${styles.col2}`}><input name="pwd" type="password" placeholder="비밀번호"   classNames="loginregister__input"/></span>
+            </div>
+            {/* <div class={styles.row}> 
+              <span class={`${styles.cell} ${styles.col2}`}><input type="submit" class={styles.btn_submit} value="회원가입" /></span>
+            </div> */}
+          </div>
+          <div>
+          <input type="submit" class={styles.btn_submit} value="회원가입" />
           </div>
         </form>
       </div>
